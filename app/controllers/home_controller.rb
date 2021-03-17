e862c462ee0675e947a659ba6bddc58a2e269248
kusuma-repo <ifaltiga.pm@gmail.com>
2021-03-17 20:17:06 +0700

    add styling to home dir

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
-   
+  skip_before_action :authenticate_user!, only: %i[ index ]
   def index
-    
+    @courses = Course.all.limit(3)
+    @recents = Course.all.limit(3).order(created_at: :desc)
   end
 
 end
